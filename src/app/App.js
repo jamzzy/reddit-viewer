dac4f808ff720a7593e35fc8c0dc9b281a28bd58
James Lim <thelimjames@gmail.com>
2022-07-12 20:41:43 -0400

    add nav, and nav icon with label

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
+import redditIcon from '../data/icons/reddit.png';
 
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <nav>
+          <img className="nav-icon" src={redditIcon} alt="reddit"/>
+          <h1>RedditViewer</h1>
+        </nav>
+      </header>
+
+      <main>
+
+      </main>
     </div>
   );
 }
