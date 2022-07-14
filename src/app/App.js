8249f71d71243769e2006c84d07addb303aff8bd
James Lim <thelimjames@gmail.com>
2022-07-13 21:05:41 -0400

    update styling so that logo will be left and search bar centers on page

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,24 @@
 
 import './App.css';
 import redditIcon from '../data/icons/reddit.png';
+import { SearchBar } from '../components/searchbar/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <header>
         <nav>
-          <img className="nav-icon" src={redditIcon} alt="reddit"/>
-          <h1>RedditViewer</h1>
+          <div className='nav-container'>
+            <img className="nav-icon" src={redditIcon} alt="reddit"/>
+            <h1>RedditViewer</h1>
+          </div>
+          <div className='nav-container'>
+            <SearchBar />
+          </div>
+          <div className='nav-container'>
+
+          </div>
+          
         </nav>
       </header>
 
