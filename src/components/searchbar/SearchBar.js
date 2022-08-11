eeed08ba8b11f5bdef9a069678f9e9c41e297d46
James Lim <thelimjames@gmail.com>
2022-08-11 18:03:48 -0400

    update magnifying glass and comment styling

--- src/components/searchbar/SearchBar.js
+++ src/components/searchbar/SearchBar.js
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import './SearchBar.css';
+import magnifyingGlass from '../../data/icons/magnifying-glass.png';
 
 export const SearchBar = () => {
     const searchInputRef = useRef();
@@ -7,9 +8,8 @@ export const SearchBar = () => {
     return (
         <form className='search-form'>
             <input type="text" className="search" ref={searchInputRef} placeholder="Search Reddit" />
-            <button type="submit" className="search-button">
-            🔎
-            </button>
+            
+            <input type='image' className='search-img-input' src={magnifyingGlass} alt="Submit Search"/>
 
         </form>
     );
