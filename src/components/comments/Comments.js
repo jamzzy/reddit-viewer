f00be3685b37c2a4f497a6cb910b5c4a2c1672b9
James Lim <thelimjames@gmail.com>
2022-08-18 16:14:02 -0400

    add implementation to integrate store with the component

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,13 +1,37 @@
 import { Comment } from '../comment/Comment';
 import './Comments.css';
+import {
+    selectComments,
+    selectIsLoadingComments,
+    loadComments
 
-export const Comments = () => {
+} from '../../store/commentsSlice/commentsSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
+export const Comments = ({postID, permalink}) => {
+    const dispatch = useDispatch();
+    const comments = useSelector(selectComments);
+    const isLoadingComments = useSelector(selectIsLoadingComments);
+
+    useEffect(() => {
+        dispatch(loadComments({postID, permalink}));
+        
+    },[dispatch, permalink, postID]);
+
+    if(isLoadingComments){
+        return <div>loading comments</div>;
+    }
+    
+    if(!comments[postID]) {
+        return null;
+    }
+
     return (
         <div className='comments-container'>
-            <Comment />
-            <Comment />
-
-            
+            {
+                comments[postID].map((comment, index) => <Comment key={index} comment={comment} />)
+            }  
         </div>
     );
 }
