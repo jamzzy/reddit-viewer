84cb1a9e52a5ed67ac454a5e18b50af7d443b94f
James Lim <thelimjames@gmail.com>
2022-08-18 16:12:37 -0400

    add comments slice reducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import posts from './postsSlice/postsSlice';
+import comments from './commentsSlice/commentsSlice';
 
 export default configureStore({
     reducer: {
         posts: posts,
-        comments: 'comments',
+        comments: comments,
     }
 });
